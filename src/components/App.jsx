68a7120da1531cf18dcab6daf6cff91036cc938c
Alexey <dogadaev142@gmail.com>
2024-04-07 15:52:48 +0300

    server

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import AppRoutes from "./AppRoutes";
+import "../styles/main.css";
+function App() {
+  return (
+    <div className="container">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
